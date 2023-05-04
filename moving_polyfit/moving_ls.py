5a465c1d76c7aa716e03a3cccaca77fe70d92bdc
Jonathan Bunton <jonathan.m.bunton@gmail.com>
2023-05-04 12:23:19 -0700

    wip

--- moving_polyfit/moving_ls.py
+++ moving_polyfit/moving_ls.py
@@ -45,62 +45,55 @@ class TrajectoryEstimator:
     '''
     State estimator based on fitting simulation traces to output data.
     '''
-    def __init__(self, ode, trajectories: list, dt: float):
+    def __init__(self, y: np.ndarray, dt: float):
         '''
         args:
-            ode : ODE object for system we are estimating state of
-            trajectories (list): list of tuples of numpy arrays, (x, y)
-                x (np array, shape (n, num_steps))
-                y (np array, shape (p, num_steps))
+            y (np array, (p, d, num_steps, N) : N sims of p-dim output (scalar here) and its d derivatives for num_steps
             dt (float) : time step between samples in trajectories
         '''
-        self.num_trajectories = len(trajectories)
-        self.num_steps = trajectories[0][0].shape[1]  # number of time steps in each simulation
-        self.n = trajectories[0][0].shape[0]  # state dimension
-        self.p = trajectories[0][1].shape[0]  # output dimension
+        self.p = y.shape[0]  # output dimension, fixed at scalar for now
+        self.d = y.shape[1]  # how many derivatives can we predict?
+        self.num_steps = y.shape[2]  # number of time steps in each simulation
+        self.num_trajectories = y.shape[3]  # number of simulations we are using
 
-        self.A_matrix = np.zeros((self.n, self.n))
-        for i in range(self.n-1):
-            self.A_matrix[i, i+1] = float(self.num_steps)*dt
+        self.dt = dt
+        self.time = np.linspace(0.0, self.num_steps*dt, num=self.num_steps, endpoint=False)
 
-        self.output_data_matrix = np.empty((self.num_steps*self.p, self.num_trajectories))
-        self.state_data_matrix = np.empty((self.num_steps*self.n, self.num_trajectories))
-        self.prediction_matrix_end = np.empty((self.n, self.num_trajectories))
+        # TODO: accomodate non-scalar outputs with flattening
+        self.estimation_matrix = np.empty((self.p*self.d, self.num_trajectories))
 
-        for i, trajectory in enumerate(trajectories):
-            self.output_data_matrix[:, i] = trajectory[1].flatten(order='F')
-            self.state_data_matrix[:, i] = trajectory[0].flatten(order='F')
-            self.prediction_matrix_end[:, i] = trajectory[0][:, -1]
-        self.compute_output_matrix()
-        self.ode = ode
-        self.nderivs = self.ode.nderivs
-        self.compute_output_predict_matrix()
+        self.build_fitting_matrix(y)
+        self.build_estimation_matrix(y)
 
-    def compute_output_predict_matrix(self):
-        self.output_prediction_matrix = np.empty((self.p*self.nderivs, self.num_trajectories))
+    def build_fitting_matrix(self, y):
+        self.fitting_matrix = np.empty((self.num_steps*self.p, self.num_trajectories))
         for i in range(self.num_trajectories):
-            self.output_prediction_matrix[:, i] = self.ode.output_derivative(None, self.state_data_matrix[-self.n:, i],
-                                                                             None)
+            self.fitting_matrix[:, i] = y[:, 0, :, i].flatten(order='F')
 
-    def compute_state_matrix(self):
-        self.state_regression_matrix = np.linalg.pinv(self.state_data_matrix)
-
-    def compute_output_matrix(self):
-        self.regression_matrix = np.linalg.pinv(self.output_data_matrix)
+    def build_estimation_matrix(self, y):
+        self.estimation_matrix = np.empty((self.p*self.d, self.num_steps, self.num_trajectories))
+        for i in range(self.num_trajectories):
+            self.estimation_matrix[:, :, i] = y[:, :, :, i].reshape((self.p*self.d, self.num_steps), order='F')
 
     def fit(self, y):
-        self.theta = np.linalg.lstsq(self.output_data_matrix, y.flatten(order='F'), rcond=None)[0]
+        self.theta = np.linalg.lstsq(self.fitting_matrix, y.flatten(order='F'), rcond=None)[0]
+        self.residuals = np.abs(self.fitting_matrix @ self.theta - y.flatten(order='F'))
         return self.theta
 
-    def fit_state(self, x):
-        self.theta_state = np.linalg.lstsq(self.state_data_matrix, x.flatten(order='F'), rcond=None)[0]
-        return self.theta_state
+    def estimate(self, t: float):
+        return self.differentiate(t, 0)
 
-    def estimate_state_thetas(self):
-        return self.prediction_matrix_end @ self.theta_state
+    def nearest_times(self, t: float, k: int):
+        idx = np.argpartition(np.abs(self.time - t), k)
+        return self.time[idx[:k]], idx[:k]
 
-    def estimate(self):
-        return self.prediction_matrix_end @ self.theta
+    def differentiate(self, t: float, q: int):
+        # extract the nearest times, linearly interpolate them
+        # this math is valid when t is inside the interval of simulation evaluations
+        # TODO: fix math for linear interp outside of interval
+        # could possibly do fancier interpolations, with cubic splines, etc.
+        times, ii = self.nearest_times(t, 2)
+        w1 = 1.0 - np.abs(times[0] - t)/self.dt
+        w2 = 1.0 - w1
 
-    def output_estimate(self):
-        return self.output_prediction_matrix @ self.theta
+        return (w1*self.estimation_matrix[q, ii[0], :] + w2*self.estimation_matrix[q, ii[1], :]) @ self.theta
