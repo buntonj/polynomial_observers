c7712f62f8300ef48b96f928f62ca41da57ba451
Jonathan Bunton <jonathan.m.bunton@gmail.com>
2023-05-04 12:23:19 -0700

    wip

--- trajectory_testing.py
+++ trajectory_testing.py
@@ -3,32 +3,26 @@ from nonlinear_system.sample_odes import LorenzSystem
 from moving_polyfit.moving_ls import PolyEstimator, TrajectoryEstimator
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.polynomial import Polynomial as P
 
-np.random.seed(666)
+np.random.seed(0)
 verbose = False
 ##############################################################
 #                     TIME  PARAMETERS                       #
 ##############################################################
-N = 4  # number of samples in a window
-window_length = 0.1  # number of seconds of trajectory in a single window of data
+N = 10  # number of samples in a window
+window_length = 0.01  # number of seconds of trajectory in a single window of data
 sampling_dt = window_length/float(N)  # computed sampling timestep
 
-integration_per_sample = 2  # how many integration timesteps should we take between output samples?
+integration_per_sample = 10  # how many integration timesteps should we take between output samples?
 integration_dt = sampling_dt/integration_per_sample
 num_sampling_steps = 500  # total number of steps taken in the
 num_integration_steps = (num_sampling_steps-1)*integration_per_sample
 
 ##############################################################
-#                   FITTING PARAMETERS                       #
-##############################################################
-num_trajectories = 3  # number of trajectories to sample for fitting
-d = 3  # degree of estimation polynomial
-
-
-##############################################################
 #                    SYSTEM PARAMETERS                       #
 ##############################################################
-n = 3  # system state dimension
+n = 2  # system state dimension
 m = 1  # control input dimension
 p = 1  # output dimension
 
@@ -38,56 +32,125 @@ m = ODE.m
 p = ODE.p
 
 
-def noise(t):
-    noise_mag = 1.0
-    # f = 100.0
-    return noise_mag*np.random.randn()  # noise_mag*np.sin(f*t)
-
-
 def control_input(t, y, x=None):
-    return np.array([0.0])  # two_dim_output_deriv(t, x, None)[1]
+    # if the system has control inputs, we can calculate them here with time-varying output or state feedback
+    return np.array([np.cos(50*t)])  # two_dim_output_deriv(t, x, None)[1]
 
 
-def generate_simulated_trajectories(sys, num, N, dt):
-    trajectories = []
-    for i in range(num):
-        print(f'Creating trajectory simulation no. {i}')
-        x = np.empty((sys.n, N))
-        y = np.empty((sys.p, N))
-        x0 = 2.0*(np.random.rand(sys.n) - 0.5)
-        # x0 = OUTPUT_DERIV(None, 10.0*(np.random.randn(n) - 0.5), None)
-        x[:, 0], y[:, 0] = sys.reset(x0, u0=None, t=0.0)
-        for t in range(N):
-            u = control_input(t*dt, y[:, t], x=x[:, t])
-            x[:, t], y[:, t] = sys.step(u, dt=dt)
-        trajectories.append((x, y))
-    return trajectories
+##############################################################
+#                   FITTING PARAMETERS                       #
+##############################################################
+# POLYNOMIALS
+# currently, we set this to one below the max we can explicitly compute (for bound purposes)
+d = ODE.nderivs-1  # degree of estimation polynomial
+
+# SIMULATIONS
+num_simulations = d
+num_sim_steps = N
+
+
+def generate_simulations(system, num_simulations: int, num_steps: int):
+    '''
+    Generates an array of simulations of system outputs and their derivatives.
+    args:
+        system (ContinuousTimeystem object): system to simulate.
+        num_simulations (int): number of simulation traces to generate
+        num_steps (int): number of steps to move forward the simulation
+    returns:
+        y (np.array) : an array of shape (p, d, num_steps, num_simulations)
+    '''
+    d = system.ode.nderivs  # how many derivatives can/will we compute?
+    y = np.empty((system.p, d, num_steps, num_simulations))
+    x = np.empty((system.n, num_steps))
+
+    for i in range(num_simulations):
+        # resetting the system for each simulation
+        t = 0.0
+        x[:, 0], _ = system.reset(np.random.rand(system.n))
+
+        # kick-start, compute initial outputs
+        u = control_input(t, y[:, 0, 0, i])
+        y[:, :, 0, i] = system.ode.output_derivative(t, x[:, 0], u)
+
+        # evolve simulation forward num_steps-1 more times
+        for j in range(1, num_steps):
+            u = control_input(t, y[:, 0, j-1, i])
+            x[:, j], _ = system.step(u)
+            t = system.t
+            y[:, :, j, i] = system.ode.output_derivative(t, x[:, j], u)
+
+    return y
+
+
+##############################################################
+#                   COMPUTING THE BOUND                      #
+##############################################################
+# this vector will be multiplied with the residuals
+l_bound = np.zeros((N, d))
+
+# the theory allows us to pick any subset of (at least) d + 1 points containing the evaluation point.
+# for simplicity, here we default to the last data points.
+num_t_points = d + 1
+eval_time = (N-1)*sampling_dt
+l_times = np.linspace((N-num_t_points)*sampling_dt, N*sampling_dt, num_t_points, endpoint=False)
+verbose_lagrange = True  # to see computation details of lagrange polynomial construction/derivatives
+
+for i in range(num_t_points):
+    # build the lagrange polynomial, which is zero at all evaluation samples except one
+    evals = np.zeros(num_t_points)
+    evals[-(1+i)] = 1.0  # we are choosing the data points that are closest to our evaluation point
+    l_i = P.fit(l_times, evals, d)
+
+    # to checking that you built the right lagrange polynomial, evaluate it at the relevant points
+    if verbose_lagrange:
+        for j in range(num_t_points):
+            print(f't = {l_times[j]:.3f}, l_i(t) = {l_i(l_times[j])}')
+
+    # for every derivative that we estimate, compute this lagrange polynomial's derivative at the estimation time
+    for q in range(d):
+        l_bound[-i, q] = np.abs(l_i.deriv(q)(eval_time))  # coefficient for i-th residual in bound
+        if verbose_lagrange:
+            print(f'|l_{i}^({q})(t)|: {l_bound[i, q]}')  # for an idea of the scale of each term
 
 
 poly_estimator = PolyEstimator(d, N, sampling_dt)
-global_thetas = False
+sim_sys = ContinuousTimeSystem(ODE, dt=sampling_dt, solver='RK45')
+simulation_data = generate_simulations(sim_sys, num_simulations, num_sim_steps)
+traj_estimator = TrajectoryEstimator(simulation_data, sim_sys.dt)
+global_thetas = False  # if true, computes the coefficients in global time rather than local time frames
 
-sim_sys = ContinuousTimeSystem(ODE, dt=integration_dt, solver='RK45')
-trajectories = generate_simulated_trajectories(sim_sys, num_trajectories, N, sampling_dt)
-print('GENERATED TRAJECTORIES.')
-traj_estimator = TrajectoryEstimator(ODE, trajectories, sampling_dt)
 
+# setting up arrays to hold the state, output, and output derivatives at all integration times
 x = np.empty((n, num_integration_steps))
 y = np.empty((p, num_integration_steps))
-residual = np.empty((p, num_integration_steps))
+y_derivs = np.empty((ODE.nderivs, num_integration_steps))
 
+# setting up arrays for errors and bounds in estimation
+residual = np.empty((N, num_sampling_steps))
+bounds = np.zeros((d, num_sampling_steps))
+residual_traj = np.empty_like(residual)
+bounds_traj = np.empty_like(bounds)
+
+# setting up arrays for variables at sampling instants (could be done with index splicing)
 y_samples = np.empty((p, num_sampling_steps))
+y_derivs_samples = np.empty((ODE.nderivs, num_sampling_steps))
 x_samples = np.empty((n, num_sampling_steps))
 u = np.empty((m, num_sampling_steps-1))
 
+# estimation and paraemters at sampling times
 theta_poly = np.empty((d+1, num_sampling_steps))
-theta_traj = np.empty((num_trajectories, num_sampling_steps))
 yhat_poly = np.empty((d+1, num_sampling_steps))
 xhat_poly = np.empty((n, num_sampling_steps))
+
+theta_traj = np.empty((num_simulations, num_sampling_steps))
+yhat_traj = np.empty((d+1, num_sampling_steps))
 xhat_traj = np.empty((n, num_sampling_steps))
 
+# time
 integration_time = np.zeros((num_integration_steps,))
 sampling_time = np.zeros((num_sampling_steps,))
+
+# initializing the ODE
 x0 = 5.0*(np.random.rand(n)-0.5)
 x[:, 0] = x0
 x_samples[:, 0] = x0
@@ -95,11 +158,7 @@ sys = ContinuousTimeSystem(ODE, x0=x0, dt=integration_dt, solver='RK45')
 y[:, 0] = sys.y
 y_samples[0, 0] = sys.y
 
-print('ESTIMATOR PROPERTIES:')
-_, s, _ = np.linalg.svd(traj_estimator.output_data_matrix)
-print('Singular values', s)
-
-
+# SIMULATION LOOP
 for t in range(1, num_sampling_steps):
     # select the control input
     u[:, t-1] = control_input(sys.t, y[:, t-1], x=x[:, t-1])
@@ -108,163 +167,96 @@ for t in range(1, num_sampling_steps):
         # compute the right "in between" index
         idx = (t-1)*integration_per_sample + i
         x[:, idx], y[:, idx] = sys.step(u[:, t-1])
+        y_derivs[:, idx] = sys.ode.output_derivative(sys.t, sys.x, u[:, t-1])
         integration_time[idx] = sys.t
 
     # sample the system
     sampling_time[t] = sys.t
-    y_samples[0, t], x_samples[:, t] = sys.y + noise(sampling_time[t]), sys.x
+    y_samples[0, t], x_samples[:, t] = sys.y, sys.x
+    y_derivs_samples[:, t] = sys.ode.output_derivative(sys.t, sys.x, u[:, t-1])
 
     if t >= N-1:
-        # POLYNOMIAL FITTING
-        if global_thetas:
-            # fit polynomial
-            theta_poly[:, t] = poly_estimator.fit(y_samples[0, t-N+1:t+1], sampling_time[t-N+1])
+        # fit polynomial, save residuals
+        theta_poly[:, t] = poly_estimator.fit(y_samples[0, t-N+1:t+1])
+        theta_traj[:, t] = traj_estimator.fit(y_samples[0, t-N+1:t+1])
+        residual_traj[:, t] = traj_estimator.residuals
+        residual[:, t] = poly_estimator.residuals
 
-            # estimate with polynomial derivatives at endpoint
-            for i in range(d+1):
-                yhat_poly[i, t] = poly_estimator.differentiate(sampling_time[t], i)
-        else:
-            # fit polynomial
-            theta_poly[:, t] = poly_estimator.fit(y_samples[0, t-N+1:t+1])
+        # estimate with polynomial derivatives at endpoint
+        for i in range(d+1):
+            yhat_poly[i, t] = poly_estimator.differentiate((N-1)*sampling_dt, i)
+            yhat_traj[i, t] = traj_estimator.differentiate((N-1)*sampling_dt, i)
 
-            # estimate with polynomial derivatives at endpoint
-            for i in range(d+1):
-                yhat_poly[i, t] = poly_estimator.differentiate((N-1)*sampling_dt, i)
+        # compute a state estimate from the derivatives
         xhat_poly[:, t] = sys.ode.invert_output(sys.t, yhat_poly[:, t], u[:, t-1])
+        xhat_traj[:, t] = sys.ode.invert_output(sys.t, yhat_traj[:, t], u[:, t-1])
 
-        # TRAJECTORY FITTING
-        theta_traj[:, t] = traj_estimator.fit(y_samples[:, t-N+1:t+1])
-        xhat_traj[:, t] = sys.ode.invert_output(t, traj_estimator.output_estimate(), u[:, t-1])
-        residual[:, t] = y_samples[:p, t] - yhat_poly[:p, t]
+        # compute a bound on derivative estimation error from residuals
+        for q in range(d):
+            bounds[q, t] = np.dot(residual[:, t], l_bound[:, q])
 
     else:
         theta_poly[:, t] = 0.0
         theta_traj[:, t] = 0.0
         yhat_poly[:, t] = 0.0
-        xhat_traj[:, t] = 0.0
+        yhat_traj[:, t] = 0.0
         residual[:, t] = 0.0
+        residual_traj[:, t] = 0.0
+        bounds[:, t] = 0
 
     if verbose:
         print(f'Completed timestep {t}, t = {sys.t:.1e}, state = {sys.x}')
 
+M = np.max(np.abs(y_derivs[min(ODE.nderivs-1, d), :]))
+global_bounds = np.empty((d,))
+for q in range(d):
+    # global_bounds[q] = (M/np.math.factorial(d+1))*np.dot(l_bound[:, q],
+    #                                                     np.linspace(0.0, (N-1)*sampling_dt, N, endpoint=True)**(d+1))
+    global_bounds[q] = (M/(np.math.factorial(d+1)))*(np.sqrt(N**2+N))*((N*sampling_dt)**(d+1))*np.max(l_bound[:, q])
+    global_bounds[q] += (M/(np.math.factorial(d-q+1)))*(((q+1)*sampling_dt)**(d-q+1))
+    bounds[q, :] += (M/(np.math.factorial(d-q+1)))*(((q+1)*sampling_dt)**(d-q+1))
 
-f = plt.figure(figsize=(12, 8))
-x0_plot = f.add_subplot((221))
-x1_plot = f.add_subplot((222))
-traj_plot = f.add_subplot((223))
-u_plot = f.add_subplot((224))
-
-x0_plot.plot(integration_time, x[0, :], linewidth=2.0, c='blue', label='truth')
-x0_plot.plot(sampling_time[N:], xhat_poly[0, N:], linewidth=2.0, c='red', linestyle='dashed', label='poly estimate')
-x0_plot.plot(sampling_time[N:], xhat_traj[0, N:], linewidth=2.0, c='green', linestyle='dashed', label='traj estimate')
-x0_plot.scatter(sampling_time, x_samples[0, :], s=20, marker='x', c='blue', label='samples')
-x0_plot.set_xlabel('time (s)')
-x0_plot.set_ylabel('x1(t)')
-x0_plot.legend()
-x0_plot.grid()
-
-x1_plot.plot(integration_time, x[1, :], linewidth=2.0, c='blue', label='truth')
-x1_plot.plot(sampling_time[N:], xhat_poly[1, N:], linewidth=2.0, c='red', linestyle='dashed', label='poly estimate')
-x1_plot.plot(sampling_time[N:], xhat_traj[1, N:], linewidth=2.0, c='green', linestyle='dashed', label='traj estimate')
-x1_plot.scatter(sampling_time, x_samples[1, :], s=50, marker='x', c='blue', label='samples')
-x1_plot.set_xlabel('time (s)')
-x1_plot.set_ylabel('x2(t)')
-x1_plot.legend()
-x1_plot.grid()
-
-traj_plot.plot(x[0, :], x[1, :], linewidth=2.0, c='blue', label='truth')
-traj_plot.plot(xhat_poly[0, N:], xhat_poly[1, N:], linewidth=2.0, c='red', linestyle='dashed', label='poly estimate')
-traj_plot.plot(xhat_traj[0, N:], xhat_traj[1, N:], linewidth=2.0, c='green',
-               linestyle='dashed', label='output traj estimate')
-traj_plot.scatter(x[0, 0], x[1, 0], s=75, marker='*', c='blue')
-traj_plot.scatter(xhat_poly[0, N], xhat_poly[1, N], s=20, marker='*', c='red')
-traj_plot.scatter(x_samples[0, :], x_samples[1, :], s=20, marker='x', c='blue')
-marg = 0.1
-traj_plot.set_xlim(x[0, :].min()-marg, x[0, :].max()+marg)
-traj_plot.set_ylim(x[1, :].min()-marg, x[1, :].max()+marg)
-traj_plot.set_xlabel('x1')
-traj_plot.set_ylabel('x2')
-traj_plot.legend()
-traj_plot.grid()
-
-u_plot.plot(sampling_time[1:], u[0, :], linewidth=2.0, c='red')
-u_plot.set_xlabel('time (s)')
-u_plot.set_ylabel('u')
-u_plot.grid()
-u_plot.set_title('Input over time')
-f.tight_layout()
-
-f2 = plt.figure(figsize=(15, 6))
-thetas_plot = f2.add_subplot(121)
-
-for i in range(d+1):
-    thetas_plot.plot(sampling_time[N:], theta_poly[i, N:], linewidth=2.0, label=f'Theta[{i}], poly')
-for i in range(num_trajectories):
-    thetas_plot.plot(sampling_time[N:], theta_traj[i, N:], linewidth=2.0, linestyle='dashed',
-                     label=f'Theta[{i}], output fit')
-thetas_plot.set_ylim(min(np.amin(theta_poly[:, N:]), np.amin(theta_traj[:, N:])),
-                     max(np.amax(theta_traj[:, N:]), np.amax(theta_poly[:, N:])))
-thetas_plot.legend()
-thetas_plot.set_title('Parameters over time')
-thetas_plot.grid()
-
-yhat_plot = f2.add_subplot(122)
-colors = ['red', 'blue', 'green', 'orange', 'pink', 'deeppink', 'violet', 'lime', 'gray', 'black']
-for i in range(n):
-    kwargs = {
-        'label': f'x[{i}] error, output poly',
-        'linewidth': 2.0,
-        'color': colors[i]
-    }
-    yhat_plot.plot(sampling_time[N:], np.abs(xhat_poly[i, N:]-x_samples[i, N:]), **kwargs)
-    kwargs = {
-        'label': f'x[{i}] error, output fit',
-        'linewidth': 2.0,
-        'color': colors[i],
-        'linestyle': 'dashed'
-    }
-    yhat_plot.plot(sampling_time[N:], np.abs(xhat_traj[i, N:]-x_samples[i, N:]), **kwargs)
-yhat_plot.legend()
-yhat_plot.grid()
-yhat_plot.set_xlabel('time')
-yhat_plot.set_ylabel('error')
-yhat_plot.set_title('State estimation errors')
-f2.tight_layout()
-
-f3 = plt.figure(figsize=(12, 6))
-sample_x_plot = f3.add_subplot(121)
-sample_y_plot = f3.add_subplot(122)
-for i, trajectory in enumerate(trajectories):
-    c = colors[i % len(colors)]
-    sample_x_plot.scatter(trajectory[0][0, 0], trajectory[0][1, 0], marker='*', s=50, color=c)
-    sample_x_plot.plot(trajectory[0][0, :], trajectory[0][1, :], linewidth=2.0, linestyle='dashed',
-                       label=f'output sample {i}', color=c)
-    sample_y_plot.plot(sampling_time[:N], trajectory[1][0, :], linewidth=2.0, linestyle='dashed',
-                       label=f'output sample {i}', color=c)
-for i in range(d+1):
-    c = colors[i % len(colors)]
-    sample_y_plot.plot(sampling_time[:N], sampling_time[:N]**float(i), linewidth=2.0, label=f'poly sample {i}', color=c)
-sample_x_plot.grid()
-sample_y_plot.grid()
-sample_x_plot.set_title('Trajectory Samples')
-sample_x_plot.set_xlabel('x[0]')
-sample_x_plot.set_ylabel('x[1]')
-sample_y_plot.set_xlabel('t')
-sample_y_plot.set_ylabel('y(t)')
-sample_y_plot.set_title('Output samples')
-f3.tight_layout()
-
-f4, axs = plt.subplots(nrows=n//4+1, ncols=min(4, n), figsize=(5*min(4, n), 5))
+f4, axs = plt.subplots(nrows=d//4+1, ncols=min(4, n), figsize=(5*min(4, n), 5))
 for i, ax in enumerate(axs.ravel()):
-    ax.plot(integration_time, x[i, :], linewidth=2.0, c='blue', label='truth')
-    ax.plot(sampling_time[N:], xhat_poly[i, N:], linewidth=2.0, c='red', linestyle='dashed', label='poly estimate')
-    ax.plot(sampling_time[N:], xhat_traj[i, N:], linewidth=2.0, c='green', linestyle='dashed',
-            label='output fit estimate')
     ax.scatter(sampling_time, x_samples[i, :], s=20, marker='x', c='blue', label='samples')
+    ax.plot(sampling_time[N:], xhat_poly[i, N:], linewidth=2.0, c='red', linestyle='dashed', label='poly estimate')
+    ax.plot(integration_time, x[i, :], linewidth=2.0, c='blue', label='truth')
     ax.set_xlabel('time (s)')
     ax.set_ylabel(f'x[{i}](t)')
     ax.legend()
     ax.grid()
 f4.tight_layout()
+
+f5, axs2 = plt.subplots(nrows=d//4+1, ncols=min(4, n),
+                        figsize=(5*min(4, d), 5))
+for i, ax in enumerate(axs2.ravel()):
+    ax.fill_between(sampling_time[N:], yhat_poly[i, N:]-bounds[i, N:], yhat_poly[i, N:]+bounds[i, N:],
+                    color='red', alpha=0.5, zorder=-1)
+    ax.scatter(sampling_time[N:], yhat_poly[i, N:], color='red', marker='1')
+    # ax.errorbar(sampling_time[N:], yhat_poly[i, N:], yerr=bounds[i, N:], color='red')
+    ax.plot(sampling_time[N:], yhat_poly[i, N:], linewidth=2.0, c='red', linestyle='dashed', label='poly estimate')
+    ax.plot(integration_time, y_derivs[i, :], linewidth=2.0, c='blue', label='truth')
+    ax.set_xlabel('time (s)')
+    ax.set_ylabel(f'y^({i})(t)')
+    ax.legend()
+    ax.grid()
+f5.tight_layout()
+
+f6, axs3 = plt.subplots(nrows=d//4+1, ncols=min(4, n),
+                        figsize=(5*min(4, d), 5))
+for i, ax in enumerate(axs3.ravel()):
+    ax.plot(sampling_time[N:], np.abs(yhat_poly[i, N:]-y_derivs_samples[i, N:]), linewidth=2.0,
+            c='red', label='poly error')
+    ax.fill_between(sampling_time[N:], np.zeros_like(sampling_time[N:]), bounds[i, N:],
+                    alpha=0.5, zorder=-1)
+    ax.plot(sampling_time[N:], np.ones_like(sampling_time[N:])*global_bounds[i], linewidth=2.0,
+            c='black', label='global bound')
+    ax.plot(sampling_time[N:], bounds[i, N:], linewidth=2.0, c='red', linestyle='dashed', label='poly bound')
+    # ax.plot(integration_time, y_derivs[i, :], linewidth=2.0, c='blue', label='truth')
+    ax.set_xlabel('time (s)')
+    ax.set_ylabel(f'y^({i})(t)')
+    ax.legend()
+    ax.grid()
+f6.tight_layout()
 
 plt.show()
